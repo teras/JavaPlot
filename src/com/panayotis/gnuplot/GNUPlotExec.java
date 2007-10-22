fe2c9c615c25cd9685f745c8dfb9c2699af0db80
teras <devnull@localhost>
2007-10-22 07:58:27 +0000

    Removed "-" from the gnuplot command as execution parameter. This should work better than with the "-" command.

--- src/com/panayotis/gnuplot/GNUPlotExec.java
+++ src/com/panayotis/gnuplot/GNUPlotExec.java
@@ -56,7 +56,7 @@ class GNUPlotExec {
             GNUPlot.getDebugger().msg("** End of plot commands **", Debug.INFO);
             
             /* It's time now to start the actual gnuplot application */
-            String [] command = { getGNUPlotPath(), "-" };
+            String [] command = { getGNUPlotPath() };
             Process proc = Runtime.getRuntime().exec(command);
             OutputStreamWriter out = new OutputStreamWriter(proc.getOutputStream());
             out.write(comms);
